10b201a162b58dcb3c91a594ff3c1640cdf59842
Leon Masuda <masudal@uci.edu>
2025-04-16 21:21:27 -0700

    Child writes, parent reads

---
+++ ch3_forkAndPipe.cpp
@@ -0,0 +1,63 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+ 
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+ 
+int main(void)
+{
+  // char write_msg[BUFFER_SIZE] = "Greetings";
+  char parent_msg[BUFFER_SIZE] = "Greetings";
+  // char read_msg[BUFFER_SIZE];
+  char read_buf[BUFFER_SIZE] = "Hello back!";
+
+  // int fd[2];
+
+  int parent_to_child[2]; 
+  pid_t pid;
+
+   /* create the pipe */
+   if (pipe(fd) == -1) {
+     fprintf(stderr,"Pipe failed");
+     return 1;
+   }
+ 
+   /* fork a child process */
+   pid = fork();
+ 
+   if (pid < 0) { /* error occurred */
+     fprintf(stderr, "Fork Failed");
+     return 1;
+   }
+ 
+   if (pid > 0) { /* parent process */
+     /* close the unused end of the pipe */
+     close(fd[READ_END]);
+     printf("PARENT: Writing message\n");
+ 
+     /* write to the pipe */
+     write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
+     printf("PARENT: Wrote message, exiting\n");
+ 
+     /* close the write end of the pipe */
+     close(fd[WRITE_END]);
+     wait(NULL); 
+   }
+   else { /* child process */
+     /* close the unused end of the pipe */
+     close(fd[WRITE_END]);
+     printf("CHILD: Reading message\n");
+ 
+     /* read from the pipe */
+     read(fd[READ_END], read_msg, BUFFER_SIZE);
+     printf("CHILD: Received: %s\n", read_msg);
+ 
+     /* close the read end of the pipe */
+     close(fd[READ_END]);
+   }
+   return 0;
+}
